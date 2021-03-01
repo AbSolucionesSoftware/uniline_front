587c7bf2ddfc1531d580e8c63ffe3f73fd866638
AldoChagollan <aldochagollan@outlook.com>
2021-03-01 13:51:21 -0600

    meta info

--- src/App.js
+++ src/App.js
@@ -5,17 +5,30 @@ import { Helmet } from 'react-helmet';
 import './styles.scss';
 import Image from '../src/images/inicio.jpg';
 
-function App() {
-	return (
-		<div className="App">
-			<Helmet>
-				<meta charSet="utf-8" />
+{
+	/* <meta charSet="utf-8" />
 				<title>UNILINE</title>
 				<meta property="og:title" content="Escuela Al Revés UNILINE" />
 				<meta property="og:description" content="Aprende en nuestra escuela en linea." />
 				<meta property="og:image" content={Image} />
-				<meta property="og:url" content="https://priceless-roentgen-d8c7ba.netlify.app" />
-			</Helmet>
+				<meta property="og:url" content="https://priceless-roentgen-d8c7ba.netlify.app" /> */
+}
+
+function App() {
+	return (
+		<div className="App">
+			<Helmet
+				title="UNILINE"
+				meta={[
+					{ property: 'og:title', content: "Escuela Al Revés UNILINE" },
+					{ property: 'og:type', content: 'website' },
+					{ property: 'og:url', content: "https://priceless-roentgen-d8c7ba.netlify.app"},
+					{ property: 'og:description', content: "Aprende en nuestra escuela en linea." },
+					{ property: 'og:image', content: image},
+
+					{ name: 'viewport', content: 'width=device-width, maximum-scale=1' },
+				]}
+			/>
 			<Router>
 				<Switch>{routes.map((route, index) => <RoutesWithSubRoutes key={index} {...route} />)}</Switch>
 			</Router>
