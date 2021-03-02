f90cb9762bf7b7b7188d80a06a9e2dad56f69c2c
AldoChagollan <aldochagollan@outlook.com>
2021-03-02 09:21:34 -0600

    meta

--- src/config/metadata.js
+++ src/config/metadata.js
@@ -1,25 +1,35 @@
-import React from 'react'
+import React from 'react';
 /* import { Helmet } from 'react-helmet' */
 import { HeadProvider, Title, Link, Meta } from 'react-head';
 import Imagen from '../images/inicio.jpg';
 
-export default function Metadata({title, ogTitle, description, image, url}) {
-    if(!image) image = Imagen;
+export default function Metadata({ title, ogTitle, description, image, url }) {
+	if (!image) image = Imagen;
 
-    return (
-        <HeadProvider>
-            <Title>{title}</Title>
-            <Meta charset="utf-8" />
-            <Link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
-            <Meta name="viewport" content="width=device-width, initial-scale=1" />
-            <Meta name="theme-color" content="#000000" />
-            <Link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-            <Link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-            
+	const meta = {
+		title: title,
+		description: 'Aprende en nuestra escuela en linea.',
+		meta: {
+			charset: 'utf-8',
+			name: {
+				keywords: 'react,meta,document,html,tags'
+			}
+		}
+	};
+	return (
+		<HeadProvider>
+			{/* <Title>{title}</Title>
+			<Meta charset="utf-8" />
+			<Link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+			<Meta name="viewport" content="width=device-width, initial-scale=1" />
+			<Meta name="theme-color" content="#000000" />
+			<Link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+			<Link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+
 			<Meta property="og:title" content={ogTitle} />
 			<Meta property="og:description" content={description} />
 			<Meta property="og:image" content={image} />
-			<Meta property="og:url" content={url} />
-        </HeadProvider>
-    )
+			<Meta property="og:url" content={url} /> */}
+		</HeadProvider>
+	);
 }
--- src/pages/users/Home/home.js
+++ src/pages/users/Home/home.js
@@ -3,22 +3,42 @@ import Banner from './Banner/banner';
 import BannerInformartivo from './Banner_Informativo/banner_informativo';
 import CursosComprados from './Cursos/cursos_estudiante';
 import CursosDisponibles from './Cursos/cursos';
-import { Box } from '@material-ui/core';
-import Metadata from '../../../config/metadata';
+/* import { Box } from '@material-ui/core'; */
+/* import Metadata from '../../../config/metadata'; */
+import DocumentMeta from 'react-document-meta';
+import Imagen from '../../../images/inicio.jpg';
 
 export default function Home() {
 	useEffect(() => {
 		window.scrollTo(0, 0);
 	}, []);
+
+	const meta = {
+		title: 'uniline',
+		description: 'Aprende en nuestra escuela en linea.',
+		meta: {
+			charset: 'utf-8',
+			name: {
+				keywords: 'react,meta,document,html,tags'
+			},
+			property: {
+				'og:title': 'uni',
+				'og:description': 'hoa hoa',
+				'og:image': Imagen,
+				'og:url': 'https://priceless-roentgen-d8c7ba.netlify.app/'
+			},
+		}
+	};
+
 	return (
-		<Box>
-			<Metadata
+		<DocumentMeta {...meta}>
+			{/* <Metadata
 				title="UNILINE"
 				ogTitle="Escuela Al Revés UNILINE"
 				description="Aprende en nuestra escuela en linea."
 				url="https://priceless-roentgen-d8c7ba.netlify.app"
 			/>
-			<Banner />
+			<Banner /> */}
 			<CursosComprados />
 			<CursosDisponibles />
 			<BannerInformartivo />
@@ -27,6 +47,6 @@ export default function Home() {
 			<ul>
 				<Link to="/compra">pagar curso</Link>
 			</ul> */}
-		</Box>
+		</DocumentMeta>
 	);
 }
