c5df8a3e9f84ed59b0b6493530604b919312b098
AldoChagollan <aldochagollan@outlook.com>
2021-04-06 15:28:45 -0500

    update rn

--- src/pages/users/Dashboard_Usuario/vimeo_react_native.js
+++ src/pages/users/Dashboard_Usuario/vimeo_react_native.js
@@ -1,7 +1,7 @@
-import React/* , { useState } */ from 'react';
+import React, { useEffect, useState } from 'react';
 import Vimeo from '@u-wave/react-vimeo';
 import { makeStyles } from '@material-ui/core';
-/* import { Helmet } from 'react-helmet'; */
+import { Helmet } from 'react-helmet';
 import { Fragment } from 'react';
 
 const useStyles = makeStyles((theme) => ({
@@ -24,23 +24,26 @@ const useStyles = makeStyles((theme) => ({
 
 export default function VimeoReactNative(props) {
 	const classes = useStyles();
-/* 	const [ update, setUpdate ] = useState(""); */
+	const [ update, setUpdate ] = useState("start");
 	const url = props.match.params.url;
 	const info_url = props.match.params.info;
 	const info_local = process.env.REACT_APP_VIMEO_RN;
+
+	useEffect(() => {
+	}, [update])
 
 	if (info_url !== info_local) return null;
 
 	return (
 		<Fragment>
-			{/* <Helmet>
+			<Helmet>
 				<title>{update}</title>
-			</Helmet> */}
+			</Helmet>
 			<div className={classes.video}>
 				<Vimeo
 					video={url}
 					autoplay={true}
-					onEnd={() => props.history.push('/vimeo_mobil_change/true')}
+					onEnd={() => setUpdate("finish")}
 					id="vimeo-player-mobile"
 					className={classes.vimeoPlayer}
 				/>
