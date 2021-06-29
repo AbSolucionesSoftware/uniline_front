df69d818609a3b02f78233f4ba5d86f2e7e447d5
poday98 <poday736.98@gmail.com>
2021-06-29 15:02:12 -0500

    Cambio Vista

--- src/pages/users/Dashboard_Usuario/contenido_dashboard.js
+++ src/pages/users/Dashboard_Usuario/contenido_dashboard.js
@@ -1,11 +1,13 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Avatar, Box, Divider, Grid, Typography, CircularProgress } from '@material-ui/core';
+import { Avatar, Box, Divider,Hidden, Grid, Typography, CircularProgress, Tab } from '@material-ui/core';
 import { List, ListItem, ListItemAvatar, ListItemIcon, ListItemText } from '@material-ui/core';
 import Rating from '@material-ui/lab/Rating';
 import DOMPurify from 'dompurify';
 import Vimeo from '@u-wave/react-vimeo';
+import PropTypes from 'prop-types';
 
+import AppBar from '@material-ui/core/AppBar';
 import InsertChartOutlinedIcon from '@material-ui/icons/InsertChartOutlined';
 import LanguageOutlinedIcon from '@material-ui/icons/LanguageOutlined';
 import SchoolOutlinedIcon from '@material-ui/icons/SchoolOutlined';
@@ -18,6 +20,7 @@ import { DashboardContext } from '../../../context/dashboar_context';
 import clienteAxios from '../../../config/axios';
 import Calificacion from './calificacion';
 import ProyectoFinal from './proyecto_final';
+import { Tabs } from 'react-bootstrap';
 
 const useStyles = makeStyles((theme) => ({
 	itemIcon: {
@@ -41,6 +44,7 @@ const useStyles = makeStyles((theme) => ({
 	}
 }));
 
+
 export default function ContenidoDashboard({ user }) {
 	const classes = useStyles();
 	const token = localStorage.getItem('token');
@@ -48,7 +52,7 @@ export default function ContenidoDashboard({ user }) {
 		DashboardContext
 	);
 	const [ cursoFinalizado, setCursoFinalizado ] = useState(false);
-
+	
 	const checkTema = async (topic) => {
 		setAction(0);
 		await clienteAxios
@@ -89,7 +93,7 @@ export default function ContenidoDashboard({ user }) {
 
 	return (
 		<div>
-			<Box
+			{/* <Box
 				id="dashboard-reproductor"
 				className={classes.video}
 				display="flex"
@@ -120,212 +124,212 @@ export default function ContenidoDashboard({ user }) {
 						className={classes.vimeoPlayer}
 					/>
 				)}
-			</Box>
-			<Box minHeight={200}>
-				<Box m={2}>
-					<Grid container justify="space-between">
-						<Grid item>
-							<Typography variant="h6">
-								{temaActual ? temaActual.tema.topicTitle : curso.course.title ? curso.course.title : ''}
-							</Typography>
-						</Grid>
-						<Grid item classes={classes.stars}>
-							<Box display="flex" justifyContent="center">
-								<Rating
-									name="read-only"
-									value={curso.course.qualification ? curso.course.qualification : 0}
-									readOnly
-									precision={0.5}
-								/>
-							</Box>
-							{!calificado ? <Calificacion curso={curso} update={update} setUpdate={setUpdate} /> : null}
-						</Grid>
-					</Grid>
-				</Box>
-				<Divider />
-				<Box p={2}>
-					<Typography variant="h5">Más información de este curso</Typography>
-					<Box mt={2}>
-						<Typography variant="subtitle1">
-							{curso.course.subtitle ? curso.course.subtitle : ''}
-						</Typography>
-					</Box>
-					<Box mt={3}>
-						<Grid container spacing={5}>
+			</Box> */}
+				<Box minHeight={200}>
+					<Box m={2}>
+						<Grid container justify="space-between">
 							<Grid item>
-								<Typography>Informacion básica</Typography>
-								<Divider />
-								<Grid container spacing={2}>
-									<Grid item>
-										<List dense>
-											<ListItem>
-												<ListItemIcon className={classes.itemIcon}>
-													<InsertChartOutlinedIcon />
-												</ListItemIcon>
-												<Typography variant="subtitle1">
-													{curso.course.level ? `Nivel: ${curso.course.level}` : 'Nivel: -'}
-												</Typography>
-											</ListItem>
-											<ListItem>
-												<ListItemIcon className={classes.itemIcon}>
-													<LanguageOutlinedIcon />
-												</ListItemIcon>
-												<Typography variant="subtitle1">
-													{curso.course.language ? (
-														`Lenguaje: ${curso.course.language}`
-													) : (
-														'Lenguaje: -'
-													)}
-												</Typography>
-											</ListItem>
-											<ListItem>
-												<ListItemIcon className={classes.itemIcon}>
-													<SchoolOutlinedIcon />
-												</ListItemIcon>
-												<Typography variant="subtitle1">Certificación al finalizar</Typography>
-											</ListItem>
-										</List>
-									</Grid>
-									<Grid item>
-										<List dense>
-											<ListItem>
-												<ListItemIcon className={classes.itemIcon}>
-													<SubscriptionsOutlinedIcon />
-												</ListItemIcon>
-												<Typography variant="subtitle1">
-													{`${curso.totalTopics} clases`}
-												</Typography>
-											</ListItem>
-											<ListItem>
-												<ListItemIcon className={classes.itemIcon}>
-													<QueryBuilderOutlinedIcon />
-												</ListItemIcon>
-												<Typography variant="subtitle1">
-													{curso.course.hours ? (
-														`${curso.course.hours} horas de curso`
-													) : (
-														'0 horas de curso'
-													)}
-												</Typography>
-											</ListItem>
-											<ListItem>
-												<ListItemIcon className={classes.itemIcon}>
-													<FolderOutlinedIcon />
-												</ListItemIcon>
-												<Typography variant="subtitle1">Trabajo final</Typography>
-											</ListItem>
-										</List>
-									</Grid>
-									<Grid item lg={4}>
-										<List dense>
-											<ListItem>
-												<ListItemAvatar>
-													{curso.course.idProfessor.urlImage ? (
-														<Avatar
-															alt="img-maestro"
-															src={curso.course.idProfessor.urlImage}
-														/>
-													) : (
-														<Avatar>P</Avatar>
-													)}
-												</ListItemAvatar>
-												<ListItemText
-													primary={
-														<Fragment>
-															<Typography>Instructor</Typography>
-															<Typography variant="subtitle1">
-																{curso.course.idProfessor.name ? (
-																	curso.course.idProfessor.name
+								<Typography variant="h6">
+									{temaActual ? temaActual.tema.topicTitle : curso.course.title ? curso.course.title : ''}
+								</Typography>
+							</Grid>
+							<Grid item classes={classes.stars}>
+								<Box display="flex" justifyContent="center">
+									<Rating
+										name="read-only"
+										value={curso.course.qualification ? curso.course.qualification : 0}
+										readOnly
+										precision={0.5}
+									/>
+								</Box>
+								{!calificado ? <Calificacion curso={curso} update={update} setUpdate={setUpdate} /> : null}
+							</Grid>
+						</Grid>
+					</Box>
+					<Divider />
+					<Box p={2}>
+						<Typography variant="h5">Más información de este curso</Typography>
+						<Box mt={2}>
+							<Typography variant="subtitle1">
+								{curso.course.subtitle ? curso.course.subtitle : ''}
+							</Typography>
+						</Box>
+						<Box mt={3}>
+							<Grid container spacing={5}>
+								<Grid item>
+									<Typography>Informacion básica</Typography>
+									<Divider />
+									<Grid container spacing={2}>
+										<Grid item>
+											<List dense>
+												<ListItem>
+													<ListItemIcon className={classes.itemIcon}>
+														<InsertChartOutlinedIcon />
+													</ListItemIcon>
+													<Typography variant="subtitle1">
+														{curso.course.level ? `Nivel: ${curso.course.level}` : 'Nivel: -'}
+													</Typography>
+												</ListItem>
+												<ListItem>
+													<ListItemIcon className={classes.itemIcon}>
+														<LanguageOutlinedIcon />
+													</ListItemIcon>
+													<Typography variant="subtitle1">
+														{curso.course.language ? (
+															`Lenguaje: ${curso.course.language}`
+														) : (
+															'Lenguaje: -'
+														)}
+													</Typography>
+												</ListItem>
+												<ListItem>
+													<ListItemIcon className={classes.itemIcon}>
+														<SchoolOutlinedIcon />
+													</ListItemIcon>
+													<Typography variant="subtitle1">Certificación al finalizar</Typography>
+												</ListItem>
+											</List>
+										</Grid>
+										<Grid item>
+											<List dense>
+												<ListItem>
+													<ListItemIcon className={classes.itemIcon}>
+														<SubscriptionsOutlinedIcon />
+													</ListItemIcon>
+													<Typography variant="subtitle1">
+														{`${curso.totalTopics} clases`}
+													</Typography>
+												</ListItem>
+												<ListItem>
+													<ListItemIcon className={classes.itemIcon}>
+														<QueryBuilderOutlinedIcon />
+													</ListItemIcon>
+													<Typography variant="subtitle1">
+														{curso.course.hours ? (
+															`${curso.course.hours} horas de curso`
+														) : (
+															'0 horas de curso'
+														)}
+													</Typography>
+												</ListItem>
+												<ListItem>
+													<ListItemIcon className={classes.itemIcon}>
+														<FolderOutlinedIcon />
+													</ListItemIcon>
+													<Typography variant="subtitle1">Trabajo final</Typography>
+												</ListItem>
+											</List>
+										</Grid>
+										<Grid item lg={4}>
+											<List dense>
+												<ListItem>
+													<ListItemAvatar>
+														{curso.course.idProfessor.urlImage ? (
+															<Avatar
+																alt="img-maestro"
+																src={curso.course.idProfessor.urlImage}
+															/>
+														) : (
+															<Avatar>P</Avatar>
+														)}
+													</ListItemAvatar>
+													<ListItemText
+														primary={
+															<Fragment>
+																<Typography>Instructor</Typography>
+																<Typography variant="subtitle1">
+																	{curso.course.idProfessor.name ? (
+																		curso.course.idProfessor.name
+																	) : (
+																		'-'
+																	)}
+																</Typography>
+															</Fragment>
+														}
+														secondary={
+															<Typography variant="subtitle2">
+																{curso.course.idProfessor.profession ? (
+																	curso.course.idProfessor.profession
 																) : (
 																	'-'
 																)}
 															</Typography>
-														</Fragment>
-													}
-													secondary={
-														<Typography variant="subtitle2">
-															{curso.course.idProfessor.profession ? (
-																curso.course.idProfessor.profession
-															) : (
-																'-'
-															)}
-														</Typography>
-													}
-												/>
-											</ListItem>
-										</List>
-									</Grid>
-									<Grid item>
-										<Box display="flex" justifyContent="center" alignItems="center" height="100%">
-											<div>
-												<ProyectoFinal curso={curso} update={update} user={user} />
-											</div>
-										</Box>
+														}
+													/>
+												</ListItem>
+											</List>
+										</Grid>
+										<Grid item>
+											<Box display="flex" justifyContent="center" alignItems="center" height="100%">
+												<div>
+													<ProyectoFinal curso={curso} update={update} user={user} />
+												</div>
+											</Box>
+										</Grid>
 									</Grid>
 								</Grid>
 							</Grid>
-						</Grid>
+						</Box>
+						<Box />
 					</Box>
-					<Box />
+					<Box p={2}>
+						<Typography variant="h6">Descripción del curso</Typography>
+						<Divider />
+						<Box p={2} textAlign="justify">
+							<Typography
+								dangerouslySetInnerHTML={{
+									__html: DOMPurify.sanitize(curso.course.description ? curso.course.description : '')
+								}}
+							/>
+						</Box>
+						<Typography variant="h6">¿Qué aprenderás?</Typography>
+						<Box px={5} py={2}>
+							<ul>
+								{curso.course.learnings ? (
+									curso.course.learnings.map((res) => {
+										return (
+											<li key={res._id}>
+												<Typography>{res.learning}</Typography>
+											</li>
+										);
+									})
+								) : null}
+							</ul>
+						</Box>
+						<Typography variant="h6">Requisitos</Typography>
+						<Box px={5} py={2}>
+							<ul>
+								{curso.course.requirements ? (
+									curso.course.requirements.map((res) => {
+										return (
+											<li key={res._id}>
+												<Typography>{res.requirement}</Typography>
+											</li>
+										);
+									})
+								) : null}
+							</ul>
+						</Box>
+						<Typography variant="h6">¿Para quién es este curso?</Typography>
+						<Box px={5} py={2}>
+							<ul>
+								{curso.course.whoStudents ? (
+									curso.course.whoStudents.map((res) => {
+										return (
+											<li key={res._id}>
+												<Typography>{res.whoStudent}</Typography>
+											</li>
+										);
+									})
+								) : null}
+							</ul>
+						</Box>
+					</Box>
 				</Box>
-				<Box p={2}>
-					<Typography variant="h6">Descripción del curso</Typography>
-					<Divider />
-					<Box p={2} textAlign="justify">
-						<Typography
-							dangerouslySetInnerHTML={{
-								__html: DOMPurify.sanitize(curso.course.description ? curso.course.description : '')
-							}}
-						/>
-					</Box>
-					<Typography variant="h6">¿Qué aprenderás?</Typography>
-					<Box px={5} py={2}>
-						<ul>
-							{curso.course.learnings ? (
-								curso.course.learnings.map((res) => {
-									return (
-										<li key={res._id}>
-											<Typography>{res.learning}</Typography>
-										</li>
-									);
-								})
-							) : null}
-						</ul>
-					</Box>
-					<Typography variant="h6">Requisitos</Typography>
-					<Box px={5} py={2}>
-						<ul>
-							{curso.course.requirements ? (
-								curso.course.requirements.map((res) => {
-									return (
-										<li key={res._id}>
-											<Typography>{res.requirement}</Typography>
-										</li>
-									);
-								})
-							) : null}
-						</ul>
-					</Box>
-					<Typography variant="h6">¿Para quién es este curso?</Typography>
-					<Box px={5} py={2}>
-						<ul>
-							{curso.course.whoStudents ? (
-								curso.course.whoStudents.map((res) => {
-									return (
-										<li key={res._id}>
-											<Typography>{res.whoStudent}</Typography>
-										</li>
-									);
-								})
-							) : null}
-						</ul>
-					</Box>
+				<Divider />
+				<Box minHeight={200}>
+					<ComentariosCurso curso={curso} />
 				</Box>
-			</Box>
-			<Divider />
-			<Box minHeight={200}>
-				<ComentariosCurso curso={curso} />
-			</Box>
 		</div>
 	);
 }
